b7c45eb4f16e2bd8e4e82df11d05b5aaa50e4e4f
PIAAR <PIAAR@rmasters.group>
2024-05-20 18:34:27 -0400

    Successful Contentful Status

--- rj-3d-portfolio/api/Contentful/api/entry.py
+++ rj-3d-portfolio/api/Contentful/api/entry.py
@@ -1,9 +1,24 @@
-from contentful import Client
+# This Python code snippet is demonstrating how to fetch a specific entry from a Contentful space
+# using the Contentful SDK. Here is a breakdown of what the code is doing:
+import contentful
+from dotenv import load_dotenv
+import os
 
-client = Client(
-    "<space_id>",
-    "<content_delivery_api_key>",
-    environment="master",  # Optional - it defaults to 'master'.
+load_dotenv()
+
+space_id = os.getenv('CONTENTFUL_SPACE_ID')
+delivery_api_key = os.getenv('CONTENTFUL_DELIVERY_API_KEY')
+entry_id = "1cSGgEq83zI1nS5v6MWX2S"  # Replace with the actual entry ID you want to fetch
+
+client = contentful.Client(
+    space_id,
+    delivery_api_key, # Replace 'master' if you are using a different environment
 )
 
-entry = client.entry("<entry_id>")
+try:
+    entry = client.entry(entry_id).firstName()
+    print(entry)
+except contentful.errors.NotFoundError as e:
+    print(f"Error: {e.message}")
+    print(f"Request ID: {e.request_id}")
+    print(f"Details: {e.details}")
